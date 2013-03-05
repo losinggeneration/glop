88ac6a995fb5a94350d687b0bba73f4aaec44d4a
Harley Laue <losinggeneration@gmail.com>
2013-03-05 15:46:58 -0600

    Include the libs after the source file for SDL

--- gos/linux/make-sdl.bash
+++ gos/linux/make-sdl.bash
@@ -1,5 +1,5 @@
 g++ -fPIC -o glop_sdl.o -c -Iinclude `pkg-config --cflags sdl` glop_sdl.cpp
-g++ -fPIC -shared -o libglop.so `pkg-config --libs sdl` glop_sdl.o
+g++ -fPIC -shared -o libglop.so glop_sdl.o `pkg-config --libs sdl`
 rm -f glop_sdl.o
 mkdir -p lib
 mv libglop.so lib
